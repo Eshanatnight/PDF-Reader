02e5080d9cf8b48fb067e30ba6c7e3034eaefd88
Eshan Chatterjee <eshan2001221@gmail.com>
2020-11-02 22:01:36 +0530

    added gui support

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-import pyttsx3
-import PyPDF2
-from extra import *
+import tkinter as Tk
+import threading
+from gui import GUI
+
+
+book = ''
 
 if __name__ == "__main__":
     print("[Starting]: Reader is starting")
-    # initializing speaker object
-    speaker = pyttsx3.init()
-    nameOfBook = str(input("Enter The Name(without extension) with the Path to the file: "))
-    book = open(nameOfBook + ".pdf", 'rb')
-    reader = PyPDF2.PdfFileReader(book)
-    speaker.setProperty('rate', 175)
-
-    print("Press 1 to start Reading \n")
-    print("Press 2 to start Reading from a specific page \n")
-
-    choice = int(input())
-
-    if choice == 1:
-        startFull(reader, speaker)
-
-    elif choice == 2:
-        pageNumber = int(input("Enter Page Number to start from: "))
-        startFrom(reader, speaker, pageNumber)
-
+    root = Tk.Tk()
+    root.title("PDF Reader")
+    root.geometry("185x170")
+    gui = GUI()
+    clear = Tk.Label(root, text=" ")
+    clear.grid(row=0, column=0, padx=15, pady=15, columnspan=2)
+    selectButton = Tk.Button(root, text="Select a PDF", padx=20, pady=15, command=gui.start)
+    selectButton.grid(row=1, column=1)
+    endButton = Tk.Button(root, text="Stop", padx=20, pady=15, command=threading.Thread(target=gui.end).start())
+    endButton.grid(row=1, column=2)
+    clear2 = Tk.Label(root, text=" ")
+    clear2.grid(row=2, column=0, padx=15, pady=15, columnspan=2)
+    root.mainloop()
+    gui.join()
     print("[Finished]: Process Finished")
+
